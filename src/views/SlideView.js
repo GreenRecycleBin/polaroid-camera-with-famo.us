82f0828cba9b4e054da28fd3c50a02f8226932d5
Daniel Le <GreenRecycleBin@gmail.com>
2014-05-24 18:51:52 +0800

    Add a film to the SlideView

--- src/views/SlideView.js
+++ src/views/SlideView.js
@@ -14,13 +14,15 @@ define(function (require, exports, module) {
     this.mainNode = this.add(this.rootModifier);
 
     _createBackground.call(this);
+    _createFilm.call(this);
   }
 
   SlideView.prototype = Object.create(View.prototype);
   SlideView.prototype.constructor = SlideView;
 
   SlideView.DEFAULT_OPTIONS = {
-    size: [400, 450]
+    size: [400, 450],
+    filmBorder: 15
   };
 
   function _createBackground() {
@@ -32,6 +34,26 @@ define(function (require, exports, module) {
     });
 
     this.mainNode.add(background);
+  }
+
+  function _createFilm() {
+    this.options.filmSize = this.options.size[0] - 2 * this.options.filmBorder;
+
+    var film = new Surface({
+      size: [this.options.filmSize, this.options.filmSize],
+      properties: {
+        backgroundColor: '#222',
+        zIndex: 1
+      }
+    });
+
+    var filmModifier = new StateModifier({
+      origin: [0.5, 0],
+      align: [0.5, 0],
+      transform: Transform.translate(0, this.options.filmBorder, 1)
+    });
+
+    this.mainNode.add(filmModifier).add(film);
   }
 
   module.exports = SlideView;
