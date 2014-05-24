736cceddd8733e4c08de7a6d70ca7b4eabf51780
Daniel Le <GreenRecycleBin@gmail.com>
2014-05-24 19:13:28 +0800

    Add a lightbox to the SlideshowView

--- src/views/SlideshowView.js
+++ src/views/SlideshowView.js
@@ -4,18 +4,34 @@ define(function (require, exports, module) {
   var Transform = require('famous/core/Transform');
   var StateModifier = require('famous/modifiers/StateModifier');
   var SlideView = require('views/SlideView');
+  var Lightbox = require('famous/views/Lightbox');
 
   function SlideshowView() {
     View.apply(this, arguments)
 
-    var slideView = new SlideView();
-    this.add(slideView);
+    this.rootModifier = new StateModifier({
+      size: this.options.size,
+      origin: [0.5, 0],
+      align: [0.5, 0]
+    });
+
+    this.mainNode = this.add(this.rootModifier);
+
+    _createLightbox.call(this);
   }
 
   SlideshowView.prototype = Object.create(View.prototype);
   SlideshowView.prototype.constructor = SlideshowView;
 
-  SlideshowView.DEFAULT_OPTIONS = {};
+  SlideshowView.DEFAULT_OPTIONS = {
+    size: [450, 500],
+    lightboxOptions: {}
+  };
+
+  function _createLightbox() {
+    this.lightbox = new Lightbox(this.options.lightboxOptions);
+    this.mainNode.add(this.lightbox);
+  }
 
   module.exports = SlideshowView;
 });
