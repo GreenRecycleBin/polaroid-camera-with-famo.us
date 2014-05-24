cc15c7dd53ad66c99cf7ebcd8479ae9c6b7c1cb4
Daniel Le <GreenRecycleBin@gmail.com>
2014-05-24 19:14:06 +0800

    Load data when app starts up
    Pass data to the AppView

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 define(function(require, exports, module) {
 	var Engine  = require('famous/core/Engine');
+	var Utility = require('famous/utilities/Utility');
 	var AppView = require('views/AppView');
+	var SlideData = require('data/SlideData');
 
 	var mainContext = Engine.createContext();
-	var appView = new AppView();
-	mainContext.add(appView);
+
+	Utility.loadURL(SlideData.getUrl(), initApp);
+
+	function initApp(data) {
+		var data = SlideData.parse(data);
+		var appView = new AppView({ data: data });
+		mainContext.add(appView);
+	}
 });
