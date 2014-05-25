57973ed200578550ca51b4a6206334ee897d5150
Daniel Le <GreenRecycleBin@gmail.com>
2014-05-25 10:50:51 +0800

    Add a camera to the SlideshowView
    Position the image at the exit of the camera

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -4,21 +4,60 @@ define(function (require, exports, module) {
   var Transform = require('famous/core/Transform');
   var StateModifier = require('famous/modifiers/StateModifier');
   var SlideshowView = require('views/SlideshowView')
+  var ImageSurface = require('famous/surfaces/ImageSurface');
 
   function AppView() {
     View.apply(this, arguments)
 
+    _createCamera.call(this);
+    _createSlideshow.call(this);
+  }
+
+  function _createCamera() {
+    var camera = new ImageSurface({
+      size: [this.options.cameraWidth, true],
+      content: 'img/camera.png',
+      properties: {
+        width: '100%'
+      }
+    });
+
+    var cameraModifier = new StateModifier({
+      origin: [0.5, 0],
+      align: [0.5, 0],
+      transform: Transform.behind
+    });
+
+    this.add(cameraModifier).add(camera);
+  }
+
+  function _createSlideshow() {
     var slideshowView = new SlideshowView({
+      size: [this.options.slideWidth, this.options.slideHeight],
       data: this.options.data
     });
-    this.add(slideshowView);
+
+    var slideshowModifier = new StateModifier({
+      origin: [0.5, 0],
+      align: [0.5, 0],
+      transform: Transform.translate(0, this.options.slidePosition, 0)
+    })
+
+    this.add(slideshowModifier).add(slideshowView);
   }
 
   AppView.prototype = Object.create(View.prototype);
   AppView.prototype.constructor = AppView;
 
+  var cameraWidth = 0.5 * window.innerHeight;
+  var slideWidth = 0.8 * cameraWidth
+
   AppView.DEFAULT_OPTIONS = {
-    data: undefined
+    data: undefined,
+    cameraWidth: cameraWidth,
+    slideWidth: slideWidth,
+    slideHeight: slideWidth + 40,
+    slidePosition: 0.77 * cameraWidth
   };
 
   module.exports = AppView;
