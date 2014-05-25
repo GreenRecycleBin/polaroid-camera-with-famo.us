1ceb022b139cdb069991c3c59db6a9036c62f478
Daniel Le <GreenRecycleBin@gmail.com>
2014-05-25 11:20:41 +0800

    Make the slides swing back and forth
    Set perspective on the slideshowContainer

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -52,6 +52,7 @@ define(function (require, exports, module) {
 
     this.add(slideshowModifier).add(slideshowContainer);
     slideshowContainer.add(slideshowView);
+    slideshowContainer.context.setPerspective(1000);
   }
 
   AppView.prototype = Object.create(View.prototype);
