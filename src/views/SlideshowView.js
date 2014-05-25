93ef4292db42ae89ddcd5edde97c092232171102
Daniel Le <GreenRecycleBin@gmail.com>
2014-05-25 11:01:02 +0800

    Add a transition to the slides
    Make them look like they’re coming out of the camera and then falling

--- src/views/SlideshowView.js
+++ src/views/SlideshowView.js
@@ -5,6 +5,7 @@ define(function (require, exports, module) {
   var StateModifier = require('famous/modifiers/StateModifier');
   var SlideView = require('views/SlideView');
   var Lightbox = require('famous/views/Lightbox');
+  var Easing = require('famous/transitions/Easing')
 
   function SlideshowView() {
     View.apply(this, arguments)
@@ -38,7 +39,17 @@ define(function (require, exports, module) {
   SlideshowView.DEFAULT_OPTIONS = {
     size: [450, 500],
     data: undefined,
-    lightboxOptions: {}
+    lightboxOptions: {
+      inOpacity: 1,
+      outOpacity: 0,
+      inOrigin: [0, 0],
+      outOrigin: [0, 0],
+      showOrigin: [0, 0],
+      inTransform: Transform.thenMove(Transform.rotateX(0.9), [0, -300, 0]),
+      outTransform: Transform.thenMove(Transform.rotateZ(0.7), [0, window.innerHeight, -1000]),
+      inTransition: { duration: 650, curve: 'easeOut' },
+      outTransition: { duration: 500, curve: Easing.inCubic }
+    }
   };
 
   function _createLightbox() {
